0b42013966e5fce39287a966ceeb8d81f1166474
wolfcomp <magnus1997.mho@gmail.com>
2024-10-08 15:12:34 +0200

    add a single system for DOTS

---
+++ Assets/InventorySystem.cs
@@ -0,0 +1,11 @@
+using Unity.Burst;
+using Unity.Entities;
+
+partial struct InventorySystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        
+    }
+}
