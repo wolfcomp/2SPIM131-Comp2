9831e0108d0f6669efe96d62cde12ef51afee11d
wolfcomp <magnus1997.mho@gmail.com>
2024-10-13 17:30:53 +0200

    fix system update call

--- Assets/Scripts/InventorySystem.cs
+++ Assets/Scripts/InventorySystem.cs
@@ -3,9 +3,14 @@ using Unity.Entities;
 
 partial struct InventorySystem : ISystem
 {
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<InventoryComponent>();
+    }
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        
+
     }
 }
