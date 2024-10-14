aa2ba6d4c1e318f8495dd0dfc89d08710c6f9f2d
wolfcomp <magnus1997.mho@gmail.com>
2024-10-14 12:29:17 +0200

    add ECS tags to player

---
+++ Assets/Scripts/Authors/PlayerAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+public class PlayerAuthoring : MonoBehaviour
+{
+    class Baker : Baker<PlayerAuthoring>
+    {
+        public override void Bake(PlayerAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent<Player>(entity);
+        }
+    }
+}
